f59f3a73809011d2e1241c7f9a7defd2d5084bf9
Andreea Badiu <andreea.badiu@softvision.ro>
2017-08-22 17:32:59 +0300

    JS added, colors.less and other changes on css

---
+++ project/assets/common.js
@@ -0,0 +1,14 @@
+
+
+const NAV = function() {
+    return `
+        <div class= "navigation-bar navigation-bar-skin">
+        <nav>
+            <img class="navigation-bar-svlogo" src="assets/images/logo.png" alt="Softvision Logo">
+            <a  class="navigation-bar-active" href="/css/">Evaluations</a>
+            <a href="/js/">New Evaluation</a>
+            <a class="navigation-bar-logout" href="/js/">Logout</a>
+        </nav>
+        </div>
+    `;
+}
